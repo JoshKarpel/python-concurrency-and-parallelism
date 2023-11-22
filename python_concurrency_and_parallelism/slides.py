ad3d61f1c642e8dac7f73b617988f36259649c78
Josh Karpel <josh.karpel@gmail.com>
2023-11-22 14:31:32 -0600

    title and you may have heard

--- python_concurrency_and_parallelism/slides.py
+++ python_concurrency_and_parallelism/slides.py
@@ -19,11 +19,15 @@ logger = get_logger()
 
 full_block = "█"
 
+python_blue = Color.from_hex("#4B8BBE")
+python_chunk = Chunk(content="Python", style=CellStyle(foreground=python_blue))
+
 
 @component
 def root() -> Div:
     slides = [
-        processes_and_threads_in_memory,
+        title,
+        you_may_have_heard,
         processes_and_threads_in_memory,
     ]
 
@@ -71,7 +75,7 @@ def footer(current_slide: int, total_slides: int) -> Div:
             Text(
                 content=[
                     Chunk(
-                        content="Python Concurrency & Parallelism",
+                        content="Concurrency & Parallelism in Python",
                     ),
                 ],
                 style=text_slate_200,
@@ -90,6 +94,84 @@ def footer(current_slide: int, total_slides: int) -> Div:
                     ),
                 ],
                 style=text_slate_200,
+            ),
+        ],
+    )
+
+
+@component
+def title() -> Div:
+    return Div(
+        style=col | align_self_center | align_children_center | justify_children_center | gap_children_2,
+        children=[
+            Text(content="Concurrency & Parallelism in Python", style=weight_none),
+            Text(content="Josh Karpel", style=weight_none),
+            Text(content="MadPy, May 2024", style=weight_none | text_gray_400),
+        ],
+    )
+
+
+@component
+def you_may_have_heard() -> Div:
+    return Div(
+        style=col | align_self_center | align_children_center | justify_children_center | gap_children_2,
+        children=[
+            Text(content="Some things you may have heard...", style=weight_none | pad_bottom_2),
+            Text(
+                content=[
+                    Chunk(content="The"),
+                    Chunk.space(),
+                    Chunk(content="Global Interpreter Lock (GIL)", style=CellStyle(foreground=red_500)),
+                    Chunk.space(),
+                    Chunk(content="makes"),
+                    Chunk.space(),
+                    python_chunk,
+                    Chunk.space(),
+                    Chunk(content="slow", style=CellStyle(foreground=red_500, underline=True)),
+                ],
+                style=weight_none,
+            ),
+            Text(
+                content=[
+                    Chunk(content="The"),
+                    Chunk.space(),
+                    Chunk(content="GIL", style=CellStyle(foreground=red_500)),
+                    Chunk.space(),
+                    Chunk(content="makes"),
+                    Chunk.space(),
+                    Chunk(content="mulithreading", style=CellStyle(foreground=green_600)),
+                    Chunk.space(),
+                    Chunk(content="useless", style=CellStyle(foreground=red_500, underline=True)),
+                    Chunk.space(),
+                    Chunk(content="in"),
+                    Chunk.space(),
+                    python_chunk,
+                ],
+                style=weight_none,
+            ),
+            Text(
+                content=[
+                    Chunk(content="(but that"),
+                    Chunk.space(),
+                    Chunk(content="doesn't matter", style=CellStyle(underline=True)),
+                    Chunk(content=", just use"),
+                    Chunk.space(),
+                    Chunk(content="multiprocessing", style=CellStyle(foreground=green_600)),
+                    Chunk(content="!)"),
+                ],
+                style=weight_none,
+            ),
+            Text(
+                content=[
+                    Chunk(content="async", style=CellStyle(foreground=green_600)),
+                    Chunk.space(),
+                    Chunk(content="makes"),
+                    Chunk.space(),
+                    python_chunk,
+                    Chunk.space(),
+                    Chunk(content="faster", style=CellStyle(foreground=green_600, underline=True)),
+                ],
+                style=weight_none,
             ),
         ],
     )
