727c92039aae390af3c8be43f5757032f5e16489
Josh Karpel <josh.karpel@gmail.com>
2024-01-28 22:44:54 -0600

    track

--- python_concurrency_and_parallelism/utils.py
+++ python_concurrency_and_parallelism/utils.py
@@ -1,31 +1,31 @@
-from counterweight.cell_paint import CellPaint
+from counterweight.elements import Chunk
 from counterweight.styles.styles import CellStyle, Color
 from more_itertools import grouper
 from structlog import get_logger
 
 logger = get_logger()
 
-black = Color.from_name("black")
+BLACK = Color.from_name("black")
 
 
 def canvas(
     width: int,
     height: int,
     cells: dict[tuple[int, int], Color],
-) -> list[CellPaint]:
-    c = []
+) -> list[Chunk]:
+    c: list[Chunk] = []
     for y_top, y_bot in grouper(range(height), 2):
-        for x in range(width):
-            c.append(
-                CellPaint(
-                    char="▀",
-                    style=CellStyle(
-                        foreground=cells.get((x, y_top), black),
-                        background=cells.get((x, y_bot), black),
-                    ),
-                )
+        c.extend(
+            Chunk(
+                content="▀",
+                style=CellStyle(
+                    foreground=cells.get((x, y_top), BLACK),
+                    background=cells.get((x, y_bot), BLACK),
+                ),
             )
-        c.append(CellPaint(char="\n"))
+            for x in range(width)
+        )
+        c.append(Chunk.newline())
     return c[:-1]  # strip off last newline
 
 
