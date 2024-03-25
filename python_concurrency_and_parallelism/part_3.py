ad32684cb32ff2bc338c309005618d853c1693bc
Josh Karpel <josh.karpel@gmail.com>
2024-03-24 22:20:17 -0500

    wip

--- python_concurrency_and_parallelism/part_3.py
+++ python_concurrency_and_parallelism/part_3.py
@@ -2,7 +2,6 @@ from textwrap import dedent
 
 from counterweight.components import component
 from counterweight.elements import Chunk, Div, Text
-from counterweight.styles import CellStyle
 from counterweight.styles.utilities import *
 
 from python_concurrency_and_parallelism.utils import drop_shadow
